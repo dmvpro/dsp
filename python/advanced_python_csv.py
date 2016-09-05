8d187a79e0527d364987ed41e86fef5f7223e23f
dmvpro <dmvjob@gmail.com>
2016-09-04 19:12:13 -0700

    Update advanced_python_csv.py
    Code for Q5

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import csv
+import urllib2
+
+# import csv file from url
+reader = csv.reader(urllib2.urlopen('https://raw.githubusercontent.com/dmvpro/dsp/master/python/faculty.csv'))
+
+next(reader) #skip header
+
+emails = [row[3] for row in reader] # extract column 4 from data
+             
+with open("emails.csv", "wb") as f:  # create export file
+    writer = csv.writer(f,delimiter="\n")  # write csv delimited by new lines
+    writer.writerows([emails])  # export file
